9f43ef185d1aa8015911db688c429c8d4d0d15a3
Jawad <e2279780@cmaisonneuve.qc.ca>
2024-05-17 18:32:28 -0400

    Ajout de la fonction de calcul et distanceMin(J'ai pas encore fini mais ça donne une idée)

--- PycharmProjects/TP2/TP2.py
+++ PycharmProjects/TP2/TP2.py
@@ -80,5 +80,63 @@ def ptittest():
     ecrireDonneesJson(fichier_json, donnees)
     print(f"données ecrites dans le fichier json")
 
-ptittest()
-#petit test pour commit
+from math import *
+import json
+import math
+
+
+# Fonction pour calculer la distance de Haversine
+def calculer_distance(lat1, lon1, lat2, lon2):
+    R = 6371.0  # Rayon de la Terre en kilomètres
+
+    # Conversion des degrés en radians
+    t1 = math.radians(lat1)
+    t2 = math.radians(lat2)
+    l1 = math.radians(lon1)
+    l2 = math.radians(lon2)
+
+    # Application de la formule de Haversine
+    dlat = t2 - t1
+    dlon = l2 - l1
+    a = math.sin(dlat / 2) ** 2 + math.cos(t1) * math.cos(t2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    distance = R * c
+
+    return distance
+# Fonction pour trouver la distance minimale
+def trouverDistanceMin(nomFichier):
+
+    with open(nomFichier, 'r', encoding='utf-8') as fichier:
+            villes = json.load(fichier)
+
+    # initialisation de la distance min a une valeure élevé
+    distance_min = float('inf')
+    ville1_min = None
+    ville2_min = None
+
+    for i in range(len(villes)):
+        for j in range(i + 1, len(villes)):
+            ville1 = villes[i]
+            ville2 = villes[j]
+            lat1, lon1 = ville1['Latitude'], ville1['Longitude']
+            lat2, lon2 = ville2['Latitude'], ville2['Longitude']
+
+            # calcul de la distance entre la ville 1 et la ville 2
+            distance = calculer_distance(lat1, lon1, lat2, lon2)
+
+            # Si la distance calculée est inférieure à la distance minimale actuelle,
+            # mettre à jour la distance minimale et les villes correspondantes
+            if distance < distance_min:
+                distance_min = distance
+                ville1_min = ville1
+                ville2_min = ville2
+
+    if ville1_min and ville2_min:
+        print(f"Distance minimale en kilomètres entre 2 villes :")
+        print(f"Ville 1 : {ville1_min['Ville']} {ville1_min['Pays']} {ville1_min['Latitude']} {ville1_min['Longitude']}")
+        print(f"Ville 2 : {ville2_min['Ville']} {ville2_min['Pays']} {ville2_min['Latitude']} {ville2_min['Longitude']}")
+        print(f"Distance en kilomètres : {distance_min:.2f}")
+
+# Exemple d'utilisation
+trouverDistanceMin('donnees.json')
+
